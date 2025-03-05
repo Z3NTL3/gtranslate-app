ad1c59cc694a96d2301c3e1b09818fe932f49978
z3ntl3 <z3ntl3discord@gmail.com>
2025-03-05 19:39:48 +0100

    03/05/2025 19:39:48

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { listen } from '@tauri-apps/api/event';
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 enum AppEvents {
     StartGlowEffect="start_glow_effect"
@@ -8,5 +9,13 @@ window.onload = () => {
     listen(AppEvents.StartGlowEffect, (_) => {
         document.getElementById("logo")?.classList.remove("glow-anim")
         setTimeout(()=> document.getElementById("logo")?.classList.add("glow-anim"), 400)
+    })
+
+    document.getElementById("header")?.addEventListener("mousedown", (event) => {
+        if (event.buttons === 1) {
+            event.detail === 2
+              ? getCurrentWindow().startDragging() 
+              : null;
+          }
     })
 };
