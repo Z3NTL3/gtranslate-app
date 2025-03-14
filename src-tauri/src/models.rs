48e369a7487653040f8e023afcdc13727ba43699
z3ntl3 <z3ntl3discord@gmail.com>
2025-03-14 21:09:50 +0100

    unfinished new updater with feedback in the gui

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 pub const START_GLOW_EFFECT: &'static str = "start_glow_effect";
 pub const WINDOW_LOADED: &'static str = "window_loaded";
 pub const EXIT: &'static str = "exit";
+pub const UPDATE_PROGRESS: &'static str = "update_progress";
+pub const UPDATE_COMPLETED: &'static str = "update_completed";
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
